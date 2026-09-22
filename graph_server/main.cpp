c31a376d1edff15f999a09d2a39e2d8ccb8b19ae
Skyy Moore <mooreapps2022@gmail.com>
2026-09-22 09:56:34 -0700

    Added basic input streams for x and y values.

--- graph_server/main.cpp
+++ graph_server/main.cpp
@@ -4,8 +4,22 @@
 int	main() {
 	std::cout
 		<< "Welcome to Early Graph Tests!"
+		<< std::endl
 		<< std::endl;
 
+	float	x_input;
+	float	y_input;
 
+	std::cout
+		<< "Enter X: ";
+
+	std::cin >> x_input;
+
+	std::cout << std::endl;
+
+	std::cout
+		<< "Enter Y: ";
+
+	std::cin >> y_input;
 	return 0;
 }
